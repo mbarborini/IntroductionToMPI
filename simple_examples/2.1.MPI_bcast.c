be63f5fefdc8a39d4b37568c295e9216260fdae5
Georgios Kafanas <g.kafanas@greeklug.gr>
2025-12-01 10:19:50 +0100

    [BUGFIX] Add library providing sleep function

--- simple_examples/2.1.MPI_bcast.c
+++ simple_examples/2.1.MPI_bcast.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <mpi.h>
 
 int main(int argc, char* argv[])
--- simple_examples/2.2.MPI_bcast_arrays.c
+++ simple_examples/2.2.MPI_bcast_arrays.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <mpi.h>
 
 #include "arrays.h"
